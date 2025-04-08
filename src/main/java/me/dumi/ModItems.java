7af766d810ea9a0a30d1c204dc7af5a500305104
NotMe <112022500+N0TM3-1@users.noreply.github.com>
2025-04-08 20:25:30 +0300

    Changed to custom template

---
+++ src/main/java/me/dumi/ModItems.java
@@ -0,0 +1,24 @@
+package me.dumi;
+
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import java.util.function.Function;
+
+public class ModItems {
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        // Create the item key.
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(StardewCraft.MOD_ID, name));
+
+        // Create the item instance.
+        Item item = itemFactory.apply(settings);
+
+        // Register the item.
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
+    }
+}
---
+++ src/main/java/me/dumi/stardewcraft/StardewCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package me.dumi.stardewcraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StardewCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
